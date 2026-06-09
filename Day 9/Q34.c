7dbabe45907a5ff5b7c251a4e4c65f7eafed8c1d
mishrautkarsh561-arch <mishrautkarsh561@gmail.com>
2026-06-09 21:47:41 +0530

    Day 9 C Programs

---
+++ Day 9/Q34.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n=5;
+    int coef;
+
+    for(i=0;i<n;i++)
+    {
+        coef = 1;
+        for(j=0;j <= i;j++)
+        {
+            printf("%d ", coef);
+            coef = coef*(i - j)/(j +1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
